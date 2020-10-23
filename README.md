118a281bbe03f125474a735211e79034b3b72a2d
Charissa Johnson <charissa.y.johnson@gmail.com>
2020-10-23 15:23:54 -0500

    add README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-micro-frontend
+
+# To Run the app run the following commands in each project
+`npm run build`
+`npm run start`
--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = React.lazy(() => import("header/Header"));
-console.log(Header);
+
 function App() {
   return (
     <div>
